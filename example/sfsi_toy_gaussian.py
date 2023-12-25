fa3bcd70d42844a6a0c4c651b62f3a232110c29c
V-Rang <venu@Venu>
2023-12-25 13:16:35 -0600

    solveAdj working in serial, parallel

--- example/sfsi_toy_gaussian.py
+++ example/sfsi_toy_gaussian.py
@@ -52,6 +52,17 @@ class PACTMisfitForm:
         # return dl.Constant(.5)/self.sigma2*ufl.inner(u*ufl.exp(m) -self.d, u*ufl.exp(m) -self.d)*ufl.dx
 
 
+class H1TikhonvFunctional:
+    def __init__(self, gamma, delta, m0):
+        self.gamma = gamma #These are dlx Constant, Expression, or Function
+        self.delta = delta
+        self.m0 = m0
+
+    def __call__(self, m): #Here m is a dlx Function
+        return ufl.inner(self.gamma * ufl.grad(m), ufl.grad(m) ) *ufl.dx + \
+               ufl.inner(self.delta * m, m)*ufl.dx
+
+
 def run_inversion(nx, ny, noise_variance, prior_param):
     sep = "\n"+"#"*80+"\n"    
 
@@ -111,7 +122,6 @@ def run_inversion(nx, ny, noise_variance, prior_param):
 
     # m_true_2 = m_true_2.vector #<class 'petsc4py.PETSc.Vec'>
     
-
     # m_true_4 = dlx.fem.Function(Vh_m)
     # m_true_4.interpolate(lambda x: np.log(0.1) + 4.*( ( ( (x[0]-2.)*(x[0]-2.) + (x[1]-2.)*(x[1]-2.) ) < 1 ) )) # <class 'dolfinx.fem.function.Function'>
     # m_true_4.x.scatter_forward()  
@@ -149,20 +159,19 @@ def run_inversion(nx, ny, noise_variance, prior_param):
     # print(m_true_fun.vector[:].min())
     # print(m_true_fun.vector[:].max())
     
-
     #M-2
-
     # print(m_true_org.min())
     # print(m_true_org.max(),'\n')
 
     # print(m_true_2.min())
     # print(m_true_2.max(),'\n')
-
     
     pde.solveFwd(u_true, x_true) #petsc4py.PETSc.Vec
 
     #to preserve values in x_true        
     x_true[hpx.STATE] = u_true
+
+
     x_true[hpx.PARAMETER] = m_fun_true.vector
 
     u_fun = hpx.vector2Function(x_true[hpx.STATE], Vh[hpx.STATE])
@@ -170,8 +179,10 @@ def run_inversion(nx, ny, noise_variance, prior_param):
     m_fun = hpx.vector2Function(x_true[hpx.PARAMETER], Vh[hpx.PARAMETER])
     m_fun.x.scatter_forward()
     
-    
-    # #LIKELIHOOD
+    # print(rank,":",min(u_fun.x.array),":",max(u_fun.x.array))
+
+
+    # LIKELIHOOD
     d = dlx.fem.Function(Vh[hpx.STATE])
     expr = u_fun * ufl.exp(m_fun)
     hpx.projection(expr,d)
@@ -180,6 +191,10 @@ def run_inversion(nx, ny, noise_variance, prior_param):
 
     misfit_form = PACTMisfitForm(d, noise_variance)
     misfit = NonGaussianContinuousMisfit(msh, Vh, misfit_form)  
+
+    #implement the gradient evaluation function before implementing the regularization
+
+    # PRIOR
 
     #works as expected
     # print(rank,":",misfit.cost(x_true))
@@ -206,10 +221,149 @@ def run_inversion(nx, ny, noise_variance, prior_param):
  
     # msh.comm.allreduce()
     # ans.scatter_reverse()
-    ans = misfit.grad(0,x_true)
 
-    print(rank,":",min(ans.array))
-    print(rank,":",max(ans.array))
+    # print(x_true[hpx.STATE].min())
+    # print(x_true[hpx.STATE].max())
+
+    # print(x_true[hpx.PARAMETER].min())
+    # print(x_true[hpx.PARAMETER].max())
+    
+    # ans = misfit.grad(0,x_true)
+
+    u_fun = hpx.vector2Function(x_true[hpx.STATE], Vh[hpx.STATE])
+    m_fun = hpx.vector2Function(x_true[hpx.PARAMETER], Vh[hpx.PARAMETER])
+    
+    # print(rank,":",len(u_fun.x.array))
+
+    # print(rank,":",u_fun.vector.min())
+    # print(rank,":",u_fun.vector.max())
+
+    # print(rank,":",m_fun.vector.min())
+    # print(rank,":",m_fun.vector.max())
+    
+    u_fun.x.scatter_forward()
+    m_fun.x.scatter_forward()
+
+    x_fun = [u_fun, m_fun]
+    x_test = [ufl.TestFunction(Vh[hpx.STATE]), ufl.TestFunction(Vh[hpx.PARAMETER])]
+    i = 0
+
+    L = dlx.fem.form(ufl.derivative( misfit_form(*x_fun), x_fun[i], x_test[i]))
+    grad = dlx.fem.petsc.create_vector(L)
+    
+    # print(rank,":",len(np.array(u_fun.x.array)),":",len(np.array(grad)))
+
+    with grad.localForm() as loc_grad:
+        loc_grad.set(0)
+    
+    dlx.fem.petsc.assemble_vector(grad,L)
+    grad.ghostUpdate(addv=petsc4py.PETSc.InsertMode.ADD_VALUES, mode=petsc4py.PETSc.ScatterMode.REVERSE)
+
+    #try to allreduce values in grad for each process to all others?
+    # create new vector of needed size (3287)
+    new_func = dlx.fem.Function(Vh[hpx.STATE])
+
+    
+    # work_sizes = [818,816,831,822]
+    
+    #But I dont know in what order/ displacement each process has the values in
+    # grad. It may not even be in contiguous blocks. - Cannot use Allgatherv :-(
+
+    # msh.comm.Allgatherv(grad,new_func.vector)
+
+
+    # print(rank,":",len(np.array(u_fun.x.array)),":",len(np.array(grad)))
+    # print(rank, ":",grad.array.min(),":",grad.array.max() ) 
+    # print(rank,":",misfit.cost(x_true))        
+
+    #Question: Are all processes suppsed to have same vector values returned from grad?
+    #No, I guess, because in normal hippylib, they dont have the same values either.
+
+    #move on for now?
+    #eval Gradient function?? -> p from adjoint, then use eval Gradient?
+    #to solveAdj, need adj_rhs. create dummy??
+
+    ##########################################
+    # adj_rhs = dlx.fem.Function(Vh_phi)
+    # adj_rhs.interpolate( lambda x: np.log(0.1) + 3.*( ( ( (x[0]-2.)*(x[0]-2.) + (x[1]-2.)*(x[1]-2.) ) < .5) ) )
+    # adj_rhs.x.scatter_forward()
+    # adj_rhs = adj_rhs.vector
+
+
+
+    # need to do something similar to assemble_vector as in the solveFwd method.
+    # ^ alternative to above
+    ##########################################
+    
+    ##########################################
+    ###doesn't work either
+    adj_rhs = pde.generate_state()
+    adj_rhs_func = hpx.vector2Function(adj_rhs,Vh[hpx.ADJOINT])
+    adj_rhs_func.interpolate( lambda x: np.log(0.1) + 3.*( ( ( (x[0]-2.)*(x[0]-2.) + (x[1]-2.)*(x[1]-2.) ) < .5) ) )
+    adj_rhs_func.x.scatter_forward()
+    adj_rhs = adj_rhs_func.vector
+
+    # print(rank,":",adj_rhs.array.min(),":",adj_rhs.array.max())
+    
+    ##########################################
+    
+    # adj_rhs.ghostUpdate(petsc4py.PETSc.InsertMode.ADD_VALUES,petsc4py.PETSc.ScatterMode.REVERSE)
+    # adj_rhs_expr = dl.Expression("std::log(0.1) + 3.*( ( ( (x[0]-2.)*(x[0]-2.) + (x[1]-2.)*(x[1]-2.) ) < .5) )", degree=1)
+    # adj_rhs = dl.interpolate(adj_rhs_expr, Vh_m).vector()
+
+    adj_true = pde.generate_state()
+    
+    # # print(type(adj_rhs))
+    # #need to get this to work in parallel - not working!
+    
+    pde.solveAdj(adj_true,x_true,adj_rhs)    
+
+    # print(rank,":",adj_rhs.array.min(),":",adj_rhs.array.max())
+    # print(rank,":",adj_true.array.min(),":",adj_true.array.max())
+
+    adj_true.ghostUpdate(petsc4py.PETSc.InsertMode.ADD_VALUES,petsc4py.PETSc.ScatterMode.REVERSE)
+    adj_true.ghostUpdate(petsc4py.PETSc.InsertMode.INSERT,petsc4py.PETSc.ScatterMode.FORWARD)
+    
+    x_true[hpx.ADJOINT] = adj_true
+
+    adj_true_func = hpx.vector2Function(adj_true,Vh[hpx.ADJOINT])  
+
+    # min_val = msh.comm.allreduce(min(adj_true_func.x.array), op=MPI.MIN)
+    # max_val = msh.comm.allreduce(max(adj_true_func.x.array), op=MPI.MAX)
+
+    # if msh.comm.rank == 0:
+    #     print(min_val, max_val)    
+
+    # print(rank,":",x_true[hpx.STATE].array.min(),":",x_true[hpx.STATE].array.max())
+    # print(rank,":",x_true[hpx.PARAMETER].array.min(),":",x_true[hpx.PARAMETER].array.max())    
+
+    # adj_fun_true = hpx.vector2Function(x_true[hpx.ADJOINT],Vh[hpx.ADJOINT])
+    # adj_fun_true.x.scatter_forward()
+
+
+    #works as intended in both serial and parallel
+    with dlx.io.XDMFFile(msh.comm, "attempt_adjoint_grad_np{0:d}_X.xdmf".format(nproc),"w") as file: #works!!
+        file.write_mesh(msh)
+        file.write_function(adj_true_func)
+
+    # print(rank,":",min(adj_fun_true.x.array),":",max(adj_fun_true.x.array))
+
+    # print(rank,":",min(adj_true.array),":",max(adj_true.array))
+    
+    # print(rank,":",min(ans.array))
+    # print(rank,":",max(ans.array))
+
+    # ans_func = hpx.vector2Function(ans,Vh[hpx.STATE])
+
+    # with dlx.io.XDMFFile(msh.comm, "attempt_misfit_grad_np{0:d}_X.xdmf".format(nproc),"w") as file: #works!!
+    #     file.write_mesh(msh)
+    #     file.write_function(ans_func)
+
+    # print(rank,":",min(ans_func.x.array))
+    # print(rank,":",max(ans_func.x.array))
+
+    # print(rank,":",min(ans.array))
+    # print(rank,":",max(ans.array))
 
     # print(dlx.fem.form(L).function_spaces[0].dofmap.index_map)
 
@@ -222,7 +376,6 @@ def run_inversion(nx, ny, noise_variance, prior_param):
     
     # ans.assemble()
     
-
     # ans = dlx.fem.petsc.assemble_vector(dlx.fem.form(L) )
     # ans = dlx.fem.petsc.assemble_vector( L )
 
@@ -232,8 +385,6 @@ def run_inversion(nx, ny, noise_variance, prior_param):
 
     # ans = dlx.fem.assemble_vector(dlx.fem.form(L)) #dolfinx.la.Vector
     # ans.scatter_reverse
-
-
 
     # print(rank,":",len(ans.array))
 
