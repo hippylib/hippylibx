5eceb62cc1adfcb59ac8d278acd62efc8928d8be
V-Rang <venu@Venu>
2024-02-04 05:46:34 -0600

    using Vector object instead of petsc4py Vec

--- hippylibX/utils/__init__.py
+++ hippylibX/utils/__init__.py
@@ -1,3 +1,3 @@
-from .vector2function import vector2Function
+from .vector2function import vector2Function, create_petsc_vector_wrap
 from .projection import projection
 from .random import parRandom
