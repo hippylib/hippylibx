7a505359760d33fa6512cc5f29b19bc4680314b2
Umberto Villa <uvilla@wustl.edu>
2024-03-19 10:08:43 -0500

    Update Regularization.py

--- hippylibX/modeling/Regularization.py
+++ hippylibX/modeling/Regularization.py
@@ -20,76 +20,83 @@ import dolfinx as dlx
 
 
 class VariationalRegularization:
-    def __init__(self, Vh : list, functional_handler, isQuadratic=False):
+    def __init__(self, Vh : dlx.FunctionSpace, functional_handler, isQuadratic=False):
         # self.Vh = Vh #Function space of the parameter.
         # self.functional_handler = functional_handler #a function or a functor that takes as input m (as dl.Function) and evaluate the regularization functional
         # self.isQuadratic = isQuadratic #Whether the functional is a quadratic form (i.e. the Hessian is constant) or not (the Hessian dependes on m
 
 
         self.Vh = Vh 
-        self.functional_handler = functional_handler #a function or a functor that takes as input m (as dl.Function) and evaluate the regularization functional
-        self.isQuadratic = isQuadratic #Whether the functional is a quadratic form (i.e. the Hessian is constant) or not (the Hessian dependes on m
-        self.xfun = [dlx.fem.Function(Vhi) for Vhi in Vh]
-        self.x_test = [ufl.TestFunction(Vh[hpx.STATE]), ufl.TestFunction(Vh[hpx.PARAMETER])]
-        
+        self.functional_handler = functional_handler #a function or a functor that takes as input m (as dlx.Function) and evaluates the regularization functional
+        self.isQuadratic = isQuadratic # Whether the functional is a quadratic form (i.e. the Hessian is constant) or not (the Hessian depends on m
+        self.mfun = dlx.fem.Function(Vh)
+        self.mtest = ufl.TestFunction(Vh)
+        self.mtrial = ufl.TrialFunction(Vh)
 
-    def cost(self,x : list):
+        self.petsc_option_M = {"ksp_type": "cg", "pc_type": "jacobi"} # see if you can set rtol atol maxiter
+        self.petsc_option_R = {"ksp_type": "cg", "pc_type": "hypre"}
+
+        self.R = None
+        self.Rsolver= None
+        
+        if(self.isQuadratic == True):
+            tmp = dlx.fem.Function(Vh).x
+            self.setLinearizationPoint(tmp)
+
+        self.M = dlx.fem.petsc.assemble( dlx.form( ufl.inner(self.mtrial, self.mtest)*ufl.dx ) )
+        self.M.assemble()
+        self.Msolver = ......
+
+    def __del__(self):
+        self.Rsolver.destroy()
+        self.R.destroy()
+        self.Msolver.destroy()
+        self.M.destroy()
+
+    def cost(self,m : dlx.la.Vector):
         
         # 1. Cast the petsc4py vector m to a dlx.Function mfun
         # 2. Compute the cost by calling assemble on self.functional_handler(mfun)
         # 3. Return the value of the cost (Make sure to call a AllReduce for parallel computations
 
-        hpx.updateFromVector(self.xfun[hpx.STATE],x[hpx.STATE])
-        u_fun = self.xfun[hpx.STATE]
-
-        hpx.updateFromVector(self.xfun[hpx.PARAMETER], x[hpx.PARAMETER])
-        m_fun = self.xfun[hpx.PARAMETER]
-        
-        loc_cost = self.functional_handler(u_fun, m_fun)
-        glb_cost_proc = dlx.fem.assemble_scalar(dlx.fem.form(loc_cost))
-        return self.Vh[hpx.STATE].mesh.comm.allreduce(glb_cost_proc, op=MPI.SUM )
+        hpx.updateFromVector(self.mfun,m)
+        cost_functional = self.functional_handler(mfun)
+        local_cost = dlx.fem.assemble_scalar(dlx.fem.form(cost_functional))
+        return self.Vh.mesh.comm.allreduce(local_cost, op=MPI.SUM )
 
 
-    def grad(self, i : int, x : list, out: dlx.la.Vector) -> dlx.la.Vector:
+    def grad(self, m: dlx.la.Vector, out: dlx.la.Vector) -> dlx.la.Vector:
 
         # 1. Cast the petsc4py vector m to a dlx.Function mfun
         # 2. call symbolic differentation of self.functional_handler(mfun) wrt mfum
         # 3. call assemble, update ghosts, and store the result in out
 
-        hpx.updateFromVector(self.xfun[hpx.STATE],x[hpx.STATE])
-        u_fun = self.xfun[hpx.STATE]
+        hpx.updateFromVector(self.mfun,m)
 
-        hpx.updateFromVector(self.xfun[hpx.PARAMETER],x[hpx.PARAMETER])
-        m_fun = self.xfun[hpx.PARAMETER]
-
-        x_fun = [u_fun, m_fun]
-        
         out.array[:] = 0.
         tmp_out = dlx.la.create_petsc_vector_wrap(out)
-        dlx.fem.petsc.assemble_vector( tmp_out, dlx.fem.form(ufl.derivative( self.functional_handler(*x_fun), x_fun[i], self.x_test[i]))  )
+        dlx.fem.petsc.assemble_vector( tmp_out, dlx.fem.form(ufl.derivative( self.functional_handler(mfun), mfun[i], self.mtest))  )
         tmp_out.ghostUpdate(petsc4py.PETSc.InsertMode.ADD_VALUES,petsc4py.PETSc.ScatterMode.REVERSE)
         tmp_out.destroy()
 
 
-    def setLinearizationPoint(self, x: list, rel_tol=1e-12, max_iter=1000):
+    def setLinearizationPoint(self, m: dlx.la.Vector, rel_tol=1e-12, max_iter=1000): #remove rel_tol, max_iter from this signature
 
         # 1. Cast the petsc4py vector m to a dlx.Function mfun
         # 2. call symbolic differentiation (twice) to get the second variation of self.functional_handler(mfun) wrt mfun
         # 3. assemble the Hessian operator (it's a sparse matrix!) in the attribute self.R
         # 4. set up a linearsolver self.Rsolver that uses CG as Krylov method, gamg as preconditioner and self.R as operator
 
-        hpx.updateFromVector(self.xfun[hpx.STATE], x[hpx.STATE])
-        u_fun = self.xfun
+        if (self.isQuadratic == True) and (self.R is not None):
+            return
 
-        hpx.updateFromVector(self.xfun[hpx.PARAMETER], x[hpx.PARAMETER])
-        m_fun = self.xfun
+        hpx.updateFromVector(self.mfun, m)
 
-        self.x_lin_fn = [u_fun, m_fun]
-
-        L = ufl.derivative(ufl.derivative(self.functional_handler(*self.x_lin_fn),m_fun,self.x_test[hpx.PARAMETER]), m_fun, self.x_test[hpx.PARAMETER])
+        L = ufl.derivative(ufl.derivative(self.functional_handler(self.mfun), self.mfun,self.mtest), self.mfun, self.mtrial)
         self.R = dlx.fem.petsc.assemble_matrix(dlx.fem.form(L))
         self.R.assemble()
         self.Rsolver = petsc4py.PETSc.KSP().create()
+        #UPDATE USING A PETSC OPTION.
         self.Rsolver.getPC().setType(petsc4py.PETSc.PC.Type.GAMG)
         self.Rsolver.setType(petsc4py.PETSc.KSP.Type.CG)
         self.Rsolver.setIterationNumber(max_iter) 
