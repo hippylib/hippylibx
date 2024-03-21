f3ec24e9eeb5ec0d5d5c3bd76636039faa0a36fa
V-Rang <venu@Venu>
2024-03-21 14:18:11 -0500

    revised random.py to produce same random numbers across runs

--- hippylibX/utils/random.py
+++ hippylibX/utils/random.py
@@ -5,14 +5,16 @@ from mpi4py import MPI
 
 class Random():
     def __init__(self, rank, nproc,seed = 1):
-       
         seed_sequence = np.random.SeedSequence(seed)
+        self.child_seeds = seed_sequence.spawn(nproc)
+        self.rank = rank
 
-        child_seeds = seed_sequence.spawn(nproc)
-        self.rng = np.random.MT19937(child_seeds[rank])
-
+    def replay(self):
+        self.rng = np.random.MT19937(self.child_seeds[self.rank])
+        
     def normal(self,sigma, out):
         num_local_values = out.index_map.size_local + out.index_map.num_ghosts
+        self.replay()
         loc_random_numbers = np.random.default_rng(self.rng).normal(loc=0,scale= sigma,size=num_local_values)
         
         out.array[:] = loc_random_numbers
@@ -20,6 +22,7 @@ class Random():
 
     def normal_perturb(self,sigma, out):
         num_local_values = out.index_map.size_local + out.index_map.num_ghosts
+        self.replay()
         loc_random_numbers = np.random.default_rng(self.rng).normal(loc=0,scale= sigma,size=num_local_values)
         out.array[:] += loc_random_numbers
         dlx.la.create_petsc_vector_wrap(out).ghostUpdate(petsc4py.PETSc.InsertMode.INSERT,petsc4py.PETSc.ScatterMode.FORWARD)
