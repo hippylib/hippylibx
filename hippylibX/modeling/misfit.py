7ee84cd7486d20ff4f3aa384b64544ca72f22a33
V-Rang <venu@Venu>
2024-03-28 23:17:44 -0500

    installing ruff in CI file

--- hippylibX/modeling/misfit.py
+++ hippylibX/modeling/misfit.py
@@ -3,70 +3,82 @@ import dolfinx as dlx
 import ufl
 from mpi4py import MPI
 import petsc4py
-import numpy as np
+
 
 class NonGaussianContinuousMisfit(object):
-    def __init__(self,Vh : list, form, bc0=[]):
+    def __init__(self, Vh: list, form, bc0=[]):
         self.Vh = Vh
         self.form = form
         self.bc0 = bc0
 
         self.x_lin_fun = None
-        self.x_test = [ufl.TestFunction(Vh[hpx.STATE]), ufl.TestFunction(Vh[hpx.PARAMETER])]
+        self.x_test = [
+            ufl.TestFunction(Vh[hpx.STATE]),
+            ufl.TestFunction(Vh[hpx.PARAMETER]),
+        ]
         self.gauss_newton_approx = False
 
         self.xfun = [dlx.fem.Function(Vhi) for Vhi in Vh]
 
-    def cost(self,x : list) -> float:
-
-        hpx.updateFromVector(self.xfun[hpx.STATE],x[hpx.STATE])
+    def cost(self, x: list) -> float:
+        hpx.updateFromVector(self.xfun[hpx.STATE], x[hpx.STATE])
         u_fun = self.xfun[hpx.STATE]
 
-        hpx.updateFromVector(self.xfun[hpx.PARAMETER],x[hpx.PARAMETER])
+        hpx.updateFromVector(self.xfun[hpx.PARAMETER], x[hpx.PARAMETER])
         m_fun = self.xfun[hpx.PARAMETER]
 
-        loc_cost = self.form(u_fun,m_fun)
+        loc_cost = self.form(u_fun, m_fun)
         glb_cost_proc = dlx.fem.assemble_scalar(dlx.fem.form(loc_cost))
-        return self.Vh[hpx.STATE].mesh.comm.allreduce(glb_cost_proc, op=MPI.SUM )
+        return self.Vh[hpx.STATE].mesh.comm.allreduce(glb_cost_proc, op=MPI.SUM)
 
-    def grad(self, i : int, x : list, out: dlx.la.Vector) -> dlx.la.Vector:
-    
-        hpx.updateFromVector(self.xfun[hpx.STATE],x[hpx.STATE])
+    def grad(self, i: int, x: list, out: dlx.la.Vector) -> dlx.la.Vector:
+        hpx.updateFromVector(self.xfun[hpx.STATE], x[hpx.STATE])
         u_fun = self.xfun[hpx.STATE]
 
-        hpx.updateFromVector(self.xfun[hpx.PARAMETER],x[hpx.PARAMETER])
+        hpx.updateFromVector(self.xfun[hpx.PARAMETER], x[hpx.PARAMETER])
         m_fun = self.xfun[hpx.PARAMETER]
 
         x_fun = [u_fun, m_fun]
-        
-        out.array[:] = 0.
+
+        out.array[:] = 0.0
         tmp_out = dlx.la.create_petsc_vector_wrap(out)
-        dlx.fem.petsc.assemble_vector( tmp_out, dlx.fem.form(ufl.derivative( self.form(*x_fun), x_fun[i], self.x_test[i]))  )
-        tmp_out.ghostUpdate(petsc4py.PETSc.InsertMode.ADD_VALUES,petsc4py.PETSc.ScatterMode.REVERSE)
-        dlx.fem.petsc.set_bc(tmp_out,self.bc0)
+        dlx.fem.petsc.assemble_vector(
+            tmp_out,
+            dlx.fem.form(ufl.derivative(self.form(*x_fun), x_fun[i], self.x_test[i])),
+        )
+        tmp_out.ghostUpdate(
+            petsc4py.PETSc.InsertMode.ADD_VALUES, petsc4py.PETSc.ScatterMode.REVERSE
+        )
+        dlx.fem.petsc.set_bc(tmp_out, self.bc0)
         tmp_out.destroy()
 
-    def setLinearizationPoint(self,x : list, gauss_newton_approx=False):
-        hpx.updateFromVector(self.xfun[hpx.STATE],x[hpx.STATE])
+    def setLinearizationPoint(self, x: list, gauss_newton_approx=False):
+        hpx.updateFromVector(self.xfun[hpx.STATE], x[hpx.STATE])
         u_fun = self.xfun[hpx.STATE]
-        hpx.updateFromVector(self.xfun[hpx.PARAMETER],x[hpx.PARAMETER])
+        hpx.updateFromVector(self.xfun[hpx.PARAMETER], x[hpx.PARAMETER])
         m_fun = self.xfun[hpx.PARAMETER]
         self.x_lin_fun = [u_fun, m_fun]
-        self.gauss_newton_approx = gauss_newton_approx 
-        
+        self.gauss_newton_approx = gauss_newton_approx
 
-    def apply_ij(self,i : int,j : int, dir : dlx.la.Vector, out : dlx.la.Vector):
-
+    def apply_ij(self, i: int, j: int, dir: dlx.la.Vector, out: dlx.la.Vector):
         form = self.form(*self.x_lin_fun)
-        if(j == hpx.STATE):
+        if j == hpx.STATE:
             dlx.fem.set_bc(dir.array, self.bc0)
-        
+
         dir_fun = hpx.vector2Function(dir, self.Vh[j])
-        action = dlx.fem.form(ufl.derivative( ufl.derivative(form, self.x_lin_fun[i], self.x_test[i]), self.x_lin_fun[j], dir_fun ))        
-        out.array[:] = 0.
+        action = dlx.fem.form(
+            ufl.derivative(
+                ufl.derivative(form, self.x_lin_fun[i], self.x_test[i]),
+                self.x_lin_fun[j],
+                dir_fun,
+            )
+        )
+        out.array[:] = 0.0
         tmp_out = dlx.la.create_petsc_vector_wrap(out)
         dlx.fem.petsc.assemble_vector(tmp_out, action)
-        tmp_out.ghostUpdate(petsc4py.PETSc.InsertMode.ADD_VALUES,petsc4py.PETSc.ScatterMode.REVERSE)
-        if(i == hpx.STATE):
-            dlx.fem.petsc.set_bc(tmp_out,self.bc0)
+        tmp_out.ghostUpdate(
+            petsc4py.PETSc.InsertMode.ADD_VALUES, petsc4py.PETSc.ScatterMode.REVERSE
+        )
+        if i == hpx.STATE:
+            dlx.fem.petsc.set_bc(tmp_out, self.bc0)
         tmp_out.destroy()
