beb97be28bbcdae810ef3375c49e9b38e7e4172e
V-Rang <venu@Venu>
2024-04-03 08:26:21 -0500

    parRandom perturb needs to be fixed

--- hippylibX/utils/random.py
+++ hippylibX/utils/random.py
@@ -15,24 +15,42 @@ class Random:
         self.rng = np.random.MT19937(self.child_seeds[self.rank])
 
     def normal(self, sigma, out):
-        num_local_values = out.index_map.size_local + out.index_map.num_ghosts
+        if(hasattr(out,"nvec")):
+            num_local_values = out[0].getLocalSize()
+        else:
+            num_local_values = out.index_map.size_local + out.index_map.num_ghosts
+        
         loc_random_numbers = np.random.default_rng(self.rng).normal(
             loc=0, scale=sigma, size=num_local_values
         )
-        out.array[:] = loc_random_numbers
-        dlx.la.create_petsc_vector_wrap(out).ghostUpdate(
-            petsc4py.PETSc.InsertMode.INSERT, petsc4py.PETSc.ScatterMode.FORWARD
-        )
+        if(hasattr(out,"nvec")): #for multivec
+            for i in range(out.nvec):
+                out[i].setArray(loc_random_numbers)                
+        else:
+            out.array[:] = loc_random_numbers
+            dlx.la.create_petsc_vector_wrap(out).ghostUpdate(
+                petsc4py.PETSc.InsertMode.INSERT, petsc4py.PETSc.ScatterMode.FORWARD
+            )
 
     def normal_perturb(self, sigma, out):
-        num_local_values = out.index_map.size_local + out.index_map.num_ghosts
+
+        if(hasattr(out,"nvec")):
+            num_local_values = out[0].getLocalSize()
+        else:
+            num_local_values = out.index_map.size_local + out.index_map.num_ghosts
+    
         loc_random_numbers = np.random.default_rng(self.rng).normal(
             loc=0, scale=sigma, size=num_local_values
         )
-        out.array[:] += loc_random_numbers
-        dlx.la.create_petsc_vector_wrap(out).ghostUpdate(
-            petsc4py.PETSc.InsertMode.INSERT, petsc4py.PETSc.ScatterMode.FORWARD
-        )
+        if(hasattr(out,"nvec")): #for multivec
+            for i in range(out.nvec):
+                # out[i] += loc_random_numbers
+                out[i].axpy(1., loc_random_numbers) #FIXME: add values to petsc Vec               
+        else:
+            out.array[:] += loc_random_numbers
+            dlx.la.create_petsc_vector_wrap(out).ghostUpdate(
+                petsc4py.PETSc.InsertMode.INSERT, petsc4py.PETSc.ScatterMode.FORWARD
+            )
 
 
 _world_rank = MPI.COMM_WORLD.rank
