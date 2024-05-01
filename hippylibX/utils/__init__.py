918ac0c6f6820eecbaa9df394237f801dda1eee3
V-Rang <venu@Venu>
2024-05-01 09:28:23 -0500

    copyright for __init__ files

--- hippylibX/utils/__init__.py
+++ hippylibX/utils/__init__.py
@@ -1,3 +1,12 @@
+# --------------------------------------------------------------------------bc-
+# Copyright (C) 2024 The University of Texas at Austin
+#
+# This file is part of the hIPPYlibx library. For more information and source
+# code availability see https://hippylib.github.io.
+#
+# SPDX-License-Identifier: GPL-2.0-only
+# --------------------------------------------------------------------------ec-
+
 from .vector2function import vector2Function, updateFromVector  # noqa
 from .random import parRandom  # noqa
 from .projection import projection  # noqa
