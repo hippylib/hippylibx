5969a773a7ca9637d7b6d84822f4d725f550c3d0
V-Rang <venu@Venu>
2024-06-26 22:49:57 -0500

    minor commit fix

--- example/3d_problem/submesh_optical_properties.py
+++ example/3d_problem/submesh_optical_properties.py
@@ -1,6 +1,6 @@
 #script to create submesh and array of optical properties that can be interpolated over the submesh
 # Input: label file(.mat), optical file (.mat), factor of reduction,
-# Output: submesh, reduced array of optical properties
+# Output: downsampled label array, submesh, downsampled optical properties array
 
 from utils import downsample_labels, submesh_maker, interpolate_optical_properties
 
@@ -8,14 +8,18 @@ factor = 4
 labels_file =  'A40210923l_label.mat'
 optical_props_file = 'A40210923l_opt.mat'
 
-downsample_label_file = 'downsample_labels.h5'
+#file to write the downsampled label array to
+downsample_label_file = 'downsample_labels.h5' 
 
 downsample_labels(factor, labels_file, downsample_label_file)
 
-submesh_file = 'submesh_3d_problem.xdmf'
+#file to write the submesh to
+submesh_file = 'submesh_3d_problem.xdmf' 
 submesh_maker(downsample_label_file, submesh_file)
 
+#file to write the downsampled optical properties array to
 downsample_optical_file = 'downsample_optical_properties.h5'
+
 interpolate_optical_properties(downsample_label_file, optical_props_file, downsample_optical_file)
 
 
