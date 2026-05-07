c74f0baf71ebd84c7ab03de4a867291a3ebe7213
Umberto Villa <uvilla@oden.utexas.edu>
2026-05-07 14:20:34 -0500

    Update test_PointwiseInterpolation.py

--- hippylibX/test/test_PointwiseInterpolation.py
+++ hippylibX/test/test_PointwiseInterpolation.py
@@ -1,29 +1,37 @@
 from mpi4py import MPI
-from dolfinx import mesh, fem
+import dolfinx as dlx
 import numpy as np
 
-# Mesh and function space
-domain = mesh.create_unit_square(MPI.COMM_WORLD, 8, 8)
-V = fem.functionspace(domain, ("Lagrange", 1))
+import hippylibX as hpx
+import unittest
 
-# Interpolation points
-x = np.array([
-    [0.2, 0.3],
-    [0.7, 0.4],
-    [0.9, 0.8],
-])
+class Testing_Execution(unittest.TestCase):
+    def test(self):
+        # Mesh and function space
+        domain = dlx.mesh.create_unit_square(MPI.COMM_WORLD, 8, 8)
+        V = dlx.fem.functionspace(domain, ("Lagrange", 1))
+    
+        # Interpolation points
+        x = np.array([
+            [0.2, 0.3],
+            [0.7, 0.4],
+            [0.9, 0.8],
+        ])
 
-# Build interpolation matrix
-P = point_interpolation_matrix(V, x)
+        # Build interpolation matrix
+        P = hpx.point_interpolation_matrix(V, x)
 
-# Example function
-u = fem.Function(V)
-u.interpolate(lambda x: x[0] + 2*x[1])
+        # Example function
+        u = dlx.fem.Function(V)
+        u.interpolate(lambda x: x[0] + 2*x[1])
 
-# Apply interpolation matrix
-u_vec = u.x.petsc_vec
+        # Apply interpolation matrix
+        u_vec = u.x.petsc_vec
 
-y = P.createVecLeft()
-P.mult(u_vec, y)
+        y = P.createVecLeft()
+        P.mult(u_vec, y)
 
-print(y.array)
+if __name__ == "__main__":
+    unittest.main()
+
+    print(y.array)
